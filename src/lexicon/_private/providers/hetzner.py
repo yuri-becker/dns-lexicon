28d0b7812114d77f02ca3c49809e425a79054382
Yuri <hi@yuri.li>
2025-10-30 13:57:43 +0100

    Rewrite hetzner provider for hetzner cloud

--- src/lexicon/_private/providers/hetzner.py
+++ src/lexicon/_private/providers/hetzner.py
@@ -3,160 +3,175 @@
 import json
 import logging
 from argparse import ArgumentParser
-from typing import List
+from typing import Any, TypedDict, cast
 
 import requests
 
-from lexicon.exceptions import AuthenticationError
+from lexicon.config import ConfigResolver
+from lexicon.exceptions import AuthenticationError, LexiconError
 from lexicon.interfaces import Provider as BaseProvider
 
 LOGGER = logging.getLogger(__name__)
 
+RrsetRecord = TypedDict("RrsetRecord", {"value": str})
+RrSet = TypedDict(
+    "RrSet",
+    {
+        "id": str,
+        "name": str,
+        "type": str,
+        "ttl": int,
+        "records": list[RrsetRecord],
+    },
+)
+CreateRecordRequest = TypedDict(
+    "CreateRecordRequest",
+    {"name": str, "type": str, "ttl": int | None, "records": list[RrsetRecord]},
+)
+SetRecordsRequest = TypedDict("SetRecordsRequest", {"records": list[RrsetRecord]})
+SetTtlRequest = TypedDict("SetTtlRequest", {"ttl": int})
+
 
 class Provider(BaseProvider):
     """
-    Implements the Hetzner DNS Provider using the service https://dns.hetzner.com.
-    It neither works for their web hosting offer "konsoleH" nor for the discontinued
-    "Domain Robot" which are supported by the ``lexicon._private.providers.hetzner_legacy`` module.
+    Provider for Hetzner Cloud DNS at https://console.hetzner.com or https://api.hetzner.cloud.
+    Does not work for konsoleH, Domain Robot, or dns.hetzner.com.
+    If you're still using dns.hetzner.com,
+    see the [official migration guide](https://docs.hetzner.com/networking/dns/migration-to-hetzner-console/process),
+    or use the `hetzner_legacy` provider.
     """
 
-    API_VERSION = "1.0"
+    API_ENDPOINT = "https://api.hetzner.cloud/v1/zones"
 
     @staticmethod
-    def get_nameservers() -> List[str]:
+    def get_nameservers() -> list[str]:
         return ["ns.hetzner.com"]
 
     @staticmethod
     def configure_parser(parser: ArgumentParser) -> None:
         parser.add_argument("--auth-token", help="Specify Hetzner DNS API token")
 
-    def __init__(self, config):
+    def __init__(self, config: ConfigResolver | dict[str, Any]):
         super(Provider, self).__init__(config)
-        self.domain_id = None
-        self.api_endpoint = "https://dns.hetzner.com/api/v1"
 
-    def authenticate(self):
-        provider = self._get_zone_by_domain(self.domain)
-        self.domain_id = provider["id"]
+    def authenticate(self) -> None:
+        self.domain_id = self._get_zone_by_domain(self.domain)["id"]
 
     def cleanup(self) -> None:
         pass
 
-    def create_record(self, rtype, name, content):
-        """
-        Creates a DNS record, if a record with type, name and content exits, do nothing
-        :returns: A json string containing the resulting record
-        :rtype: str
-        """
-
-        data = {
-            # hetzner needs the FQDN name if it does not belong to the managed domain itself
-            "name": self._get_record_name(self.domain, name),
-            "type": rtype,
-            "value": content,
-            "zone_id": self.domain_id,
-        }
-        if self._get_lexicon_option("ttl"):
-            data["ttl"] = self._get_lexicon_option("ttl")
-
-        records = self.list_records(rtype=rtype, name=name, content=content)
+    def create_record(self, rtype: str, name: str, content: str) -> bool:
+        records = self.list_records(rtype, name, content)
         if len(records) >= 1:
             for record in records:
-                LOGGER.warning(
-                    "Duplicate record %s %s %s with id %s",
-                    rtype,
-                    name,
-                    content,
-                    record["id"],
+                LOGGER.info(
+                    f"Record {rtype} {name} {content} already exists and has id {record['id']}",
                 )
             return True
-        self._post("/records", data)
+
+        ttl = self._get_lexicon_option("ttl")
+        self._post(
+            f"/{self.domain_id}/rrsets",
+            cast(
+                dict[str, Any],
+                CreateRecordRequest(
+                    name=self._get_record_name(self.domain, name),
+                    type=rtype,
+                    ttl=int(ttl) if ttl else None,
+                    records=[RrsetRecord(value=name)],
+                ),
+            ),
+        )
         return True
 
-    def list_records(self, rtype=None, name=None, content=None):
-        """
-        List all records, filterable by type, name and content
-        :rtype: list
-        :returns: list of records, might be empty
-        """
-        filter_obj = {"zone_id": self.domain_id}
-        payload = self._get("/records", filter_obj)
-        records = map(self._hetzner_record_to_lexicon_record, payload["records"])
-        filtered_records = self._filter_records(
-            records, rtype, name if name is not None else None, content
-        )
-
-        return filtered_records
-
-    def _filter_records(self, records, rtype=None, name=None, content=None):
+    def list_records(
+        self,
+        rtype: str | None = None,
+        name: str | None = None,
+        content: str | None = None,
+    ) -> list[dict[str, Any]]:
+        rrsets: list[RrSet] = self._get(f"/{self.domain_id}/rrsets")["rrsets"]
         return [
-            record
-            for record in records
-            if (rtype is None or record["type"] == rtype)
-            and (name is None or record["name"] == self._full_name(name))
-            and (content is None or record["content"] == content)
+            {
+                "id": rrset["id"],
+                "name": self._full_name(rrset["name"]),
+                "content": record["value"],
+                "type": rrset["type"],
+                "ttl": rrset["ttl"],
+            }
+            for rrset in rrsets
+            for record in rrset["records"]
+            if (rtype is None or rrset["type"] == rtype)
+            and (
+                name is None or self._full_name(rrset["name"]) == self._full_name(name)
+            )
+            and (content is None or record["value"] == content)
         ]
 
-    def update_record(self, identifier, rtype=None, name=None, content=None):
-        """
-        Create or update a record.
-        :rtype: bool
-        :returns: Boolean indicating success
-        """
-        data = {
-            "type": rtype,
-            "name": self._get_record_name(self.domain, name),
-            "value": content,
-            "zone_id": self.domain_id,
-        }
-        if self._get_lexicon_option("ttl"):
-            data["ttl"] = self._get_lexicon_option("ttl")
-        update_identifier = identifier
-        if update_identifier is None:
-            records = self.list_records(rtype, name)
-            if len(records) == 1:
-                update_identifier = records[0]["id"]
-            elif len(records) < 1:
-                raise Exception(
-                    "No records found matching type, name and content - won't update"
-                )
-            else:
-                raise Exception(
-                    "Multiple records found matching type, name and content - won't update"
-                )
-        self._put(f"/records/{update_identifier}", data)
+    def update_record(
+        self,
+        identifier: str | None = None,
+        rtype: str | None = None,
+        name: str | None = None,
+        content: str | None = None,
+    ) -> bool:
+        if name is None:
+            raise LexiconError("Cannot update record - name has to be set.")
+        if rtype is None:
+            raise LexiconError("Cannot update record - rtype has to be set.")
+
+        rrset_name = self._get_record_name(self.domain, name)
+        if content is not None:
+            self._post(
+                f"/{self.domain_id}/rrsets/{rrset_name}/{rtype}/actions/set_records",
+                cast(
+                    dict[str, Any],
+                    SetRecordsRequest(records=[RrsetRecord(value=content)]),
+                ),
+            )
+
+        ttl = self._get_lexicon_option("ttl")
+        if ttl is not None:
+            self._post(
+                f"/{self.domain_id}/rrsets/{rrset_name}/{rtype}/actions/change_ttl",
+                cast(dict[str, Any], SetTtlRequest(ttl=int(ttl))),
+            )
+
         return True
 
-    def delete_record(self, identifier=None, rtype=None, name=None, content=None):
-        """
-        Delete an existing record. If record does not exist, do nothing.
-        :rtype: bool
-        :returns: Boolean indicating success
-        """
-        delete_record_ids = []
-        if identifier is None:
-            records = self.list_records(rtype, name, content)
-            delete_record_ids = [record["id"] for record in records]
-        else:
-            delete_record_ids.append(identifier)
+    def delete_record(
+        self,
+        identifier: str | None = None,
+        rtype: str | None = None,
+        name: str | None = None,
+        content: str | None = None,
+    ) -> bool:
+        if name is None:
+            raise LexiconError("Cannot delete record - name has to be set.")
+        if rtype is None:
+            raise LexiconError("Cannot delete record - rtype has to be set.")
 
-        for record_id in delete_record_ids:
-            self._delete(f"/records/{record_id}")
+        rrset_name = self._get_record_name(self.domain, name)
+        self._delete(f"/{self.domain_id}/rrsets/{rrset_name}/{rtype}")
         return True
 
     # Helpers
-    def _request(self, action="GET", url="/", data=None, query_params=None):
-        if data is None:
-            data = {}
-        if query_params is None:
-            query_params = {}
+    def _request(
+        self,
+        action: str = "GET",
+        url: str = "/",
+        data: dict[str, Any] | None = {},
+        query_params: dict[str, Any] | None = None,
+    ):
+        data = data or {}
+        query_params = query_params or {}
         response = requests.request(
             action,
-            self.api_endpoint + url,
+            self.API_ENDPOINT + url,
             params=query_params,
             data=json.dumps(data),
             headers={
-                "Auth-API-Token": self._get_provider_option("auth_token"),
+                "Authorization": f"Bearer {self._get_provider_option('auth_token')}",
                 "Content-Type": "application/json",
             },
         )
@@ -164,49 +179,34 @@ class Provider(BaseProvider):
         response.raise_for_status()
         return response.json()
 
-    def _get_zone_by_domain(self, domain):
+    def _get_zone_by_domain(self, domain: str) -> dict[str, Any]:
         """
-        Requests all dns zones from your Hetzner account and searches for a specific
-        one to determine the ID of it
+        Requests the zone object for the given domain name
         :param domain: Name of domain for which dns zone should be searched
         :rtype: dict
-        :return: The dictionary of the zone with ``domain`` in the 'name' key
+        :return: "zone" field of the response at docs.hetzner.cloud/reference/cloud#zones-get-a-zone
         :raises Exception: If no zone was found
         :raises KeyError, ValueError: If the response is malformed
-        :raises urllib.error.HttpError: If request to /zones did not return 200
+        :raises urllib.error.HttpError: If request to /zones/domain did not return 200
         """
-        filter_obj = {"name": domain}
-        payload = self._get("/zones", filter_obj)
-        zones = payload["zones"]
-        for zone in zones:
-            if zone["name"] == domain:
-                return zone
-        raise AuthenticationError(f"No zone was found in account matching {domain}")
+        try:
+            return self._get(f"/{domain}")["zone"]
+        except requests.HTTPError as err:
+            if err.response.status_code == 401:
+                raise AuthenticationError()
+            elif err.response.status_code == 404:
+                raise LexiconError(f"There is no zone for {domain}.")
+            else:
+                raise LexiconError(err)
 
-    def _get_record_name(self, domain, record_name):
+    def _get_record_name(self, domain: str, record_name: str) -> str:
         """
         Get the name attribute appropriate for hetzner api. This means it's the name
         without domain name if record name ends with managed domain name else a fqdn
         :param domain: Name of domain for which dns zone should be searched
         :param record_name: The record name to convert
-        :rtype: str
         :return: The record name in an appropriate format for hetzner api
         """
         if record_name.rstrip(".").endswith(domain):
             record_name = self._relative_name(record_name)
         return record_name
-
-    @staticmethod
-    def _pretty_json(data):
-        return json.dumps(data, sort_keys=True, indent=4, separators=(",", ": "))
-
-    def _hetzner_record_to_lexicon_record(self, hetzner_record):
-        lexicon_record = {
-            "id": hetzner_record["id"],
-            "name": self._full_name(hetzner_record["name"]),
-            "content": hetzner_record["value"],
-            "type": hetzner_record["type"],
-        }
-        if "ttl" in hetzner_record:
-            lexicon_record["ttl"] = hetzner_record["ttl"]
-        return lexicon_record
